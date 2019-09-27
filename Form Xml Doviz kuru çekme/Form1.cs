9705dc4606e46f743cd170fe9cbaefef71db5a24
hamza çakmak <hamzaacakmak>
2019-09-27 13:56:52 +0300

    first commit

---
+++ Form Xml Doviz kuru çekme/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Form_Xml_Doviz_kuru_çekme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {           
+            string bugun = "https://www.tcmb.gov.tr/kurlar/today.xml";
+            var xmldoc = new XmlDocument();
+            xmldoc.Load(bugun);
+
+            DateTime tarih = Convert.ToDateTime(xmldoc.SelectSingleNode("//Tarih_Date").Attributes["Tarih"].Value);
+
+            string usd = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='USD']/BanknoteSelling").InnerXml;
+            label1.Text = string.Format("Tarih {0} USD; {1}", tarih.ToShortDateString(), usd);
+
+            string euro = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='EUR']/BanknoteSelling").InnerXml;
+            label2.Text = string.Format("Tarih {0} EURO; {1}", tarih.ToShortDateString(), euro);
+
+            string pound = xmldoc.SelectSingleNode("Tarih_Date/Currency [@Kod='GBP']/BanknoteSelling").InnerXml;
+            label3.Text = string.Format("Tarih {0} POUND; {1}", tarih.ToShortDateString(), pound);
+        }
+    }
+}
